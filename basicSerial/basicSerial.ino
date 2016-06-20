387eadb5a2bee7ca165fbfef7d31eaa950a5c704
Allan Rodríguez Dávila <adavilaitcr@users.noreply.github.com>
2016-06-20 15:49:32 -0600

    Add files via upload

---
+++ basicSerial/basicSerial.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(A2, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //cada cuatro microsegundos
+  Serial.print(analogRead(A0));
+  Serial.print(",");
+  Serial.println(analogRead(A2));
+  //delay(10);
+}
